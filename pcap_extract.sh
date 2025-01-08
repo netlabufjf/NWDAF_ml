2c8196062428a79cbac84221c590475e817111d9
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-01-08 19:41:22 -0300

    feat(pcap_extract): add visual progress status

--- pcap_extract.sh
+++ pcap_extract.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-PCAP_FOLDER=./pcap
+PCAP_FOLDER=./pcap/input/
 OUT_FOLDER=./pcap/output/
 
 PCAP_LIST=$(ls $PCAP_FOLDER | grep .pcap)
+COUNTER=0
+PCAP_LIST_SIZE=$(wc -w <<< "$PCAP_LIST")
 
 # PCAP to JSON and CSV
+echo "[INFO] Exporting PCAP files"
 for i in ${PCAP_LIST[@]}; do
     tshark -r $PCAP_FOLDER$i -T json > $OUT_FOLDER/"$i"_output".json"
     
@@ -16,13 +19,24 @@ for i in ${PCAP_LIST[@]}; do
 
      # To customize the "-e flags" (display filters), see https://www.wireshark.org/docs/dfref/
      # For more information: https://manpages.ubuntu.com/manpages/jammy/man1/tshark.1.html
+
+    ((COUNTER+=1))
+    PROGRESS=$(bc <<< "scale=2;$COUNTER*100/$PCAP_LIST_SIZE")
+    echo "[INFO] Status: $COUNTER of $PCAP_LIST_SIZE ($PROGRESS %)"
 done
 unset PCAP_LIST # clean up after usage
 
 JSON_LIST=$(ls $OUT_FOLDER | grep .json)
+JSON_LIST_SIZE=$(wc -w <<< "$JSON_LIST")
+COUNTER=0
 
 # Drop duplicated fields on JSON
+echo "[INFO] Removing JSON duplicated entries"
 for i in ${JSON_LIST[@]}; do
     sed -i '/"ip.addr":/d; /"ip.host":/d' $OUT_FOLDER/$i
+
+    ((COUNTER+=1))
+    PROGRESS=$(bc <<< "scale=2;$COUNTER*100/$JSON_LIST_SIZE")
+    echo "[INFO] Status: $COUNTER of $JSON_LIST_SIZE ($PROGRESS %)"
 done
 unset JSON_LIST # clean up after usage
