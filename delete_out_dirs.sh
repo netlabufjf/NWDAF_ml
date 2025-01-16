c885ec10a6859930b9fd488f48b96858706aae99
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-01-16 10:52:32 -0300

    feat: add a helper script to automatically clear the out dirs

---
+++ delete_out_dirs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo -e "[WARN] ALL the data in the output folders will be \033[31mDELETED\033[0m !"
+read -p "Press ENTER to continue or Ctrl+C to abort now"
+
+echo "[INFO] Removing the output directory"
+rm -rf ./pcap/output
+
+echo "[INFO] Reconstructing the output directory structure"
+mkdir -p ./pcap/output/{1-PCAP-export,2-stats/graphs/}
+echo "[INFO] Done"
