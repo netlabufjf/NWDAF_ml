ebe2b15eb5753d418d6b0a5f26d8e3e4a354d095
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-01-18 21:42:39 -0300

    perf(pcap_extract): Parallelize the tasks to enhance performance
    According to my tests, this parallelization can improve performance up to 66%
    
    The tasks are independent because they never write to the same output file

--- pcap_extract.sh
+++ pcap_extract.sh
@@ -13,12 +13,11 @@ PCAP_LIST_SIZE=$(wc -w <<< "$PCAP_LIST")
 time { # track execution time
 echo "[INFO] Exporting $PCAP_LIST_SIZE PCAP files"
 for i in ${PCAP_LIST[@]}; do
-    tshark -r $PCAP_FOLDER$i -T json > $OUT_FOLDER/"${i%.*}.json"
-    
+    tshark -r $PCAP_FOLDER$i -T json > $OUT_FOLDER/"${i%.*}.json" && \
     tshark -r $PCAP_FOLDER$i -T fields \
     -e frame.number -e frame.time_relative -e ip.src -e ip.dst -e _ws.col.protocol -e frame.len -e _ws.col.info \
     -E header=y -E separator=, -E quote=d -E occurrence=f \
-     > $OUT_FOLDER/"${i%.*}.csv"
+     > $OUT_FOLDER/"${i%.*}.csv" &
 
      # To customize the "-e flags" (display filters), see https://www.wireshark.org/docs/dfref/
      # For more information: https://manpages.ubuntu.com/manpages/jammy/man1/tshark.1.html
@@ -27,6 +26,7 @@ for i in ${PCAP_LIST[@]}; do
     PROGRESS=$(bc <<< "scale=2;$COUNTER*100/$PCAP_LIST_SIZE")
     echo "[INFO] Status: $COUNTER of $PCAP_LIST_SIZE ($PROGRESS %)"
 done
+wait
 unset PCAP_LIST # clean up after usage
 
 JSON_LIST=$(ls $OUT_FOLDER | grep .json)
@@ -36,12 +36,13 @@ COUNTER=0
 # Drop duplicated fields on JSON
 echo "[INFO] Removing JSON duplicated entries"
 for i in ${JSON_LIST[@]}; do
-    sed -i '/"ip.addr":/d; /"ip.host":/d' $OUT_FOLDER/$i
+    sed -i '/"ip.addr":/d; /"ip.host":/d' $OUT_FOLDER/$i &
 
     ((COUNTER+=1))
     PROGRESS=$(bc <<< "scale=2;$COUNTER*100/$JSON_LIST_SIZE")
     echo "[INFO] Status: $COUNTER of $JSON_LIST_SIZE ($PROGRESS %)"
 done
+wait
 unset JSON_LIST # clean up after usage
 
 echo "[DEBUG] Execution time:"
