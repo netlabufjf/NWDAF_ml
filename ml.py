5e71d3fec4e9660db4d54d22a3ab16f195b55e89
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-01-25 15:17:41 -0300

    fix(ml): update categorical_data_to_dummy input parameter

--- ml.py
+++ ml.py
@@ -53,8 +53,7 @@ def read_and_label_data(file_path, out_dir, remove_old_files=False):
     else:
         print(f"[INFO] The file {file_name_without_path} was already labeled")
 
-def categorical_data_to_dummy(file_path):
-    df = read_csv(file_path)
+def categorical_data_to_dummy(df):
     columns_to_exclude = ["Source_IP", "Destination_IP"] # TODO deal with these columns later (or leave them excluded)
     df = df.drop(columns=columns_to_exclude)
     df_dummies = pd.get_dummies(df)
