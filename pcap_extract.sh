7404764c5378dafac107539f4effe8c6372f3045
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-01-28 15:16:59 -0300

    fix(pcap_extract): undo the modifications regarding output dirs
    Instead of using directories to differentiate the training data from the inference data use the file names

--- pcap_extract.sh
+++ pcap_extract.sh
@@ -2,8 +2,7 @@
 
 TRAINING_PCAP_FOLDER=./pcap/input/training_data/ # read training raw PCAP files from here
 INFERENCE_PCAP_FOLDER=./pcap/input/inference_data/ # read inference raw PCAP files from here
-TRAINING_OUT_FOLDER=./pcap/output/1-PCAP-export/training/ # save the training output there
-INFERENCE_OUT_FOLDER=./pcap/output/1-PCAP-export/inference/ # save the inference output there 
+OUT_FOLDER=./pcap/output/1-PCAP-export/ # save the output there
 
 # set IFS to break only on new line (so the input files can have white space on their names)
 # for more info: https://www.linuxquestions.org/questions/programming-9/bash-put-output-from-%60ls%60-into-an-array-346719/#post1765355
@@ -23,14 +22,15 @@ extract_JSON_and_CSV () {
     local FILE_NAME=$1
     local PCAP_FOLDER=$2
     local OUT_FOLDER=$3
+    local SET_SPLIT_TAG=$4
 
     echo "[INFO] Extracting data from $FILE_NAME"
 
-    tshark -r $PCAP_FOLDER$FILE_NAME -T json > $OUT_FOLDER/"${FILE_NAME%.*}.json" && \
+    tshark -r $PCAP_FOLDER$FILE_NAME -T json > $OUT_FOLDER/"${FILE_NAME%.*}_$SET_SPLIT_TAG.json" && \
     tshark -r $PCAP_FOLDER$FILE_NAME -T fields \
     -e frame.number -e frame.time_relative -e ip.src -e ip.dst -e _ws.col.protocol -e frame.len -e _ws.col.info \
     -E header=y -E separator=, -E quote=d -E occurrence=f \
-    > $OUT_FOLDER/"${FILE_NAME%.*}.csv"
+    > $OUT_FOLDER/"${FILE_NAME%.*}_$SET_SPLIT_TAG.csv"
     # To customize the "-e flags" (display filters), see https://www.wireshark.org/docs/dfref/
     # For more information: https://manpages.ubuntu.com/manpages/jammy/man1/tshark.1.html
 }
@@ -49,12 +49,12 @@ time { # track execution time
 echo "[INFO] Exporting $TRAINING_PCAP_LIST_SIZE training and $INFERENCE_PCAP_LIST_SIZE inference PCAP files"
 if [ $TRAINING_PCAP_LIST_SIZE -ne 0 ]; then
     for i in "${TRAINING_PCAP_LIST[@]}"; do
-        extract_JSON_and_CSV "${i##*/}" $TRAINING_PCAP_FOLDER $TRAINING_OUT_FOLDER &
+        extract_JSON_and_CSV "${i##*/}" $TRAINING_PCAP_FOLDER $OUT_FOLDER "training" &
     done
 fi
 if [ $INFERENCE_PCAP_LIST_SIZE -ne 0 ]; then
     for j in "${INFERENCE_PCAP_LIST[@]}"; do
-        extract_JSON_and_CSV "${j##*/}" $INFERENCE_PCAP_FOLDER $INFERENCE_OUT_FOLDER &
+        extract_JSON_and_CSV "${j##*/}" $INFERENCE_PCAP_FOLDER $OUT_FOLDER "inference" &
     done
 fi
 wait
@@ -62,27 +62,21 @@ unset TRAINING_PCAP_LIST # clean up after usage
 unset INFERENCE_PCAP_LIST_SIZE # clean up after usage
 echo "[INFO] All $TOTAL_LIST_SIZE files have been successfully exported"
 
-TRAINING_JSON_LIST=("$TRAINING_OUT_FOLDER"*.json)
-INFERENCE_JSON_LIST=("$INFERENCE_OUT_FOLDER"*.json)
-TRAINING_JSON_LIST_SIZE=${#TRAINING_JSON_LIST[@]}
-INFERENCE_JSON_LIST_SIZE=${#INFERENCE_JSON_LIST[@]}
-TOTAL_LIST_SIZE=$((TRAINING_JSON_LIST_SIZE + INFERENCE_JSON_LIST_SIZE))
+JSON_LIST=("$OUT_FOLDER"*.json)
+JSON_LIST_SIZE=${#JSON_LIST[@]}
 
 # Drop duplicated fields on JSON
-echo "[INFO] Removing JSON duplicated entries from $TOTAL_LIST_SIZE files"
-if [ $TRAINING_JSON_LIST_SIZE -ne 0 ]; then
-    for i in "${TRAINING_JSON_LIST[@]}"; do
-        field_remover "${i##*/}" $TRAINING_OUT_FOLDER &
+echo "[INFO] Removing duplicated entries from $JSON_LIST_SIZE JSON files"
+if [ $JSON_LIST_SIZE -ne 0 ]; then
+    for i in "${JSON_LIST[@]}"; do
+        field_remover "${i##*/}" $OUT_FOLDER &
     done
-fi
-if [ $INFERENCE_JSON_LIST_SIZE -ne 0 ]; then
-    for j in "${INFERENCE_JSON_LIST[@]}"; do
-        field_remover "${j##*/}" $INFERENCE_OUT_FOLDER &
-    done
+else
+    echo "[ERRO] No JSON files found in the directory: $OUT_FOLDER"
+    exit 1
 fi
 wait
-unset TRAINING_JSON_LIST # clean up after usage
-unset INFERENCE_JSON_LIST # clean up after usage
+unset JSON_LIST # clean up after usage
 echo "[INFO] All $TOTAL_LIST_SIZE files have been processed"
 
 echo "[DEBUG] Execution time:"
