bf61f977e6b2c202cbcf6af944163a525a498b06
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-01-30 16:52:18 -0300

    fix(ml): calculate extra evaluation metrics for all models

--- ml.py
+++ ml.py
@@ -182,17 +182,16 @@ def train_models(model, X_train, X_test, y_train, y_test):
     accuracy = accuracy_score(y_test, y_pred)
     precision_avg = precision_score(y_test, y_pred, average="weighted")
     recall_avg = recall_score(y_test, y_pred, average="weighted")
-    f_score = f1_score(y_test, y_pred, average="weighted")
+    f_score_avg = f1_score(y_test, y_pred, average="weighted")
 
     print("[INFO] Accuracy:", round(accuracy, 10)) # TODO calculate more metrics for all models
     print(f"[DEBU] Confusion Matrix:\n{cm}") # TODO plot and save this matrix
+    print("[INFO] Precision :", round(precision_avg, 10))
+    print("[INFO] Recall    :", round(recall_avg, 10))
+    print("[INFO] F1-score  :", round(f_score_avg, 10))
+    print("[INFO] F1-score/class :", f1_score(y_test, y_pred, average=None, labels=[0, 1, 2]))
 
     if (model_name == 'DecisionTreeClassifier'):
-        print("[INFO] Precision :", round(precision_avg, 10))
-        print("[INFO] Recall    :", round(recall_avg, 10))
-        print("[INFO] F1-score  :", round(f_score, 10))
-        print("[INFO] F1-score/class :", f1_score(y_test, y_pred, average=None, labels=[0, 1, 2]))
-
         # Record feature importance for Decision Tree
         importance_with_columns = pd.DataFrame({'feature': X_train.columns, 'importance': model.feature_importances_})
         importance_with_columns.sort_values(by='importance', ascending=False, inplace=True, ignore_index=True)
