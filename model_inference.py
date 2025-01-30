328fb11e926cda87684112aeb7b7edb00bea6eca
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-01-30 08:20:58 -0300

    refactor(model_inference): move code to the run_inference function

--- model_inference.py
+++ model_inference.py
@@ -26,65 +26,68 @@ def label_id_to_text(id):
         exit()
     return label
 
-for file in inference_data_files:
-    file_name = file.split('/')[-1]
+def run_inference(models_file_list, inference_data_file_list):
+    for file in inference_data_file_list:
+        file_name = file.split('/')[-1]
 
-    if 'inference' in file:
-        print("[INFO] Running inference on", file_name)
-        
-        # Initialize results_df with columns
-        results_df = pd.DataFrame(columns=["file_name", "model_name", "inference_result_label", "inference_result", "inference_result_count_0", "inference_result_count_1", "inference_result_count_2"])
-        
-        data = read_csv(file) # load inference data
+        if 'inference' in file:
+            print("[INFO] Running inference on", file_name)
+            
+            # Initialize results_df with columns
+            results_df = pd.DataFrame(columns=["file_name", "model_name", "inference_result_label", "inference_result", "inference_result_count_0", "inference_result_count_1", "inference_result_count_2"])
+            
+            data = read_csv(file) # load inference data
 
-        for path in pkl_files:
-            model = pickle.load(open(path, 'rb')) # load model from disk
-            model_name = model.__class__.__name__ 
-            print("[INFO] Using", model_name)
-            
-            inference_data = data.drop('label', axis=1)  # remove the label column
-            
-            # LinearSVC doesn't calculate probabilities
-            if (model_name != 'LinearSVC'):
-                pred_proba = model.predict_proba(inference_data)  # get prediction probabilities for each class
+            for path in models_file_list:
+                model = pickle.load(open(path, 'rb')) # load model from disk
+                model_name = model.__class__.__name__ 
+                print("[INFO] Using", model_name)
                 
-                # calculate the maximum probability for each sample and its corresponding class
-                max_prob_idx = np.argmax(pred_proba, axis=1)
-                max_class = pred_proba[np.arange(len(pred_proba)), max_prob_idx]  # get the maximum probability for each sample
-                inference_result = np.array([model.classes_[i] for i in max_prob_idx])  # get the class corresponding to the maximum probability index
+                inference_data = data.drop('label', axis=1)  # remove the label column
                 
-                # count the occurrences of each predicted class
-                inference_result_counts = pd.Series(inference_result).value_counts()
+                # LinearSVC doesn't calculate probabilities
+                if (model_name != 'LinearSVC'):
+                    pred_proba = model.predict_proba(inference_data)  # get prediction probabilities for each class
+                    
+                    # calculate the maximum probability for each sample and its corresponding class
+                    max_prob_idx = np.argmax(pred_proba, axis=1)
+                    max_class = pred_proba[np.arange(len(pred_proba)), max_prob_idx]  # get the maximum probability for each sample
+                    inference_result = np.array([model.classes_[i] for i in max_prob_idx])  # get the class corresponding to the maximum probability index
+                    
+                    # count the occurrences of each predicted class
+                    inference_result_counts = pd.Series(inference_result).value_counts()
+                    
+                    # print("[DEBU] Inference result:", inference_result_counts.idxmax())  # DEBUG
+                    # print("[DEBU] Labels and their occurrences:\n", inference_result_counts)  # DEBUG
+                    
+                else:
+                    y_pred = model.predict(inference_data)
+                    inference_result = pd.Series([model.classes_[i] for i in y_pred])
+                    inference_result_counts = inference_result.value_counts()
+                    # print("[DEBU] Inference result:", inference_result.idxmax())  # DEBUG
                 
-                # print("[DEBU] Inference result:", inference_result_counts.idxmax())  # DEBUG
-                # print("[DEBU] Labels and their occurrences:\n", inference_result_counts)  # DEBUG
-                
-            else:
-                y_pred = model.predict(inference_data)
-                inference_result = pd.Series([model.classes_[i] for i in y_pred])
-                inference_result_counts = inference_result.value_counts()
-                # print("[DEBU] Inference result:", inference_result.idxmax())  # DEBUG
-            
-            inference_result_label = label_id_to_text(inference_result_counts.idxmax())
+                inference_result_label = label_id_to_text(inference_result_counts.idxmax())
 
-            new_row = {
-                "file_name": file_name,
-                "model_name": model_name,
-                "inference_result_label": inference_result_label,
-                "inference_result": inference_result_counts.idxmax(),
-                "inference_result_count_0": inference_result_counts[0] if 0 in inference_result_counts else np.nan,
-                "inference_result_count_1": inference_result_counts[1] if 1 in inference_result_counts else np.nan,
-                "inference_result_count_2": inference_result_counts[2] if 2 in inference_result_counts else np.nan
-            }
+                new_row = {
+                    "file_name": file_name,
+                    "model_name": model_name,
+                    "inference_result_label": inference_result_label,
+                    "inference_result": inference_result_counts.idxmax(),
+                    "inference_result_count_0": inference_result_counts[0] if 0 in inference_result_counts else np.nan,
+                    "inference_result_count_1": inference_result_counts[1] if 1 in inference_result_counts else np.nan,
+                    "inference_result_count_2": inference_result_counts[2] if 2 in inference_result_counts else np.nan
+                }
+                
+                # Add new row to the dataframe using loc[] method
+                results_df.loc[len(results_df)] = new_row
+                
+            # Save the results dataframe to a CSV file with a unique filename based on the current time
+            timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+            # get the original input file name without format
+            output_filename = file_name.split('/')[-1].split('_inference_')[0]
+            results_df.to_csv(f"{results_folder}{output_filename}_{timestamp}_inference_results.csv", index=False)
             
-            # Add new row to the dataframe using loc[] method
-            results_df.loc[len(results_df)] = new_row
-            
-        # Save the results dataframe to a CSV file with a unique filename based on the current time
-        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-        # get the original input file name without format
-        output_filename = file_name.split('/')[-1].split('_inference_')[0]
-        results_df.to_csv(f"{results_folder}{output_filename}_{timestamp}_inference_results.csv", index=False)
-        
-    else:
-        print(f"[WARN] Skipping file {file_name}")
+        else:
+            print(f"[WARN] Skipping file {file_name}")
+
+run_inference(pkl_files, inference_data_files)
