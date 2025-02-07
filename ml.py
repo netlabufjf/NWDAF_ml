421a490a2b1e0ccf8fdfe7e880231d053b220874
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-02-07 19:32:27 -0300

    refactor(ml): move data preprocess and labelling functions to util
    This makes it easier to import those functions on inference thus making it more flexible

--- ml.py
+++ ml.py
@@ -20,7 +20,7 @@ from sklearn.metrics import (accuracy_score,
                             roc_auc_score,
                             confusion_matrix)
 
-from util import glob_get_files_list,read_csv,delete_files,plot_confusion_matrix
+from util import glob_get_files_list,read_csv,delete_files,plot_confusion_matrix,preprocess_data,read_and_label_data
 
 # File paths
 working_folder = "./pcap/output/4-ML/"
@@ -37,40 +37,6 @@ def save_model_locally(model, file_name, out_dir):
     pickle.dump(model, open(file_path, 'wb'))
     model_name = model.__class__.__name__
     print(f"[INFO] Model {model_name} sucessfully saved on {file_path}")
-
-# Function to read and label data in CSV files
-def read_and_label_data(file_path, out_dir, remove_old_files=False):
-    # Read the CSV file
-    file_name_without_path = file_path.split('/')[-1] # get old file name
-    new_file_name_and_path = out_dir + os.path.splitext(file_name_without_path)[0] + "_labeled.csv"
-    if not os.path.exists(new_file_name_and_path):
-        df = pd.read_csv(file_path)
-        
-        # Assign labels based on filenames
-        if 'embb' in file_path:
-            label = 0
-        elif 'urllc' in file_path:
-            label = 1
-        elif 'mmtc' in file_path:
-            label = 2
-        else:
-            raise ValueError(f"Could not determine label from filename {file_path}")
-            exit()
-        
-        
-        # Add label to DataFrame
-        print(f"[INFO] Labeling {file_name_without_path} ... ", end='')
-        df['label'] = label
-        df.to_csv(new_file_name_and_path, index=False)
-        print("[ OK ]")
-
-        # if storage space is constrained, update remove_old_files to True to free some space
-        if (remove_old_files):
-            delete_files(file_name_without_path, file_path)
-        
-        print("[DEBU] Labeled data successfully saved to", new_file_name_and_path) # DEBUG
-    else:
-        print(f"[INFO] The file {file_name_without_path} was already labeled")
 
 # Load and prepare splits from labeled training data
 def read_train_data(csv_file_list):
@@ -95,73 +61,6 @@ def read_train_data(csv_file_list):
 
     return X_train, X_test, y_train, y_test
     
-def preprocess_data(files_path, output_dir):
-    columns_preprocess_time_df = ["file_name", "file_num_rows", "preprocess_time_ms", "preprocess_disk_time_ms", "preprocess_total_time_ms"]
-    preprocess_time_df = pd.DataFrame(columns=columns_preprocess_time_df) # df to save the results
-
-    for i in files_path:
-        file_name = i.split('/')[-1]
-        out_file_path = output_dir + file_name
-        preprocess_time_begin = time_ns()
-
-        if not os.path.exists(out_file_path):
-            df = read_csv(i)
-            data_num_rows = len(df)
-            print("[INFO] Working with", file_name)
-
-            # Drop some data
-            # df = df.dropna(axis=1, how='all') # drop columns where all values are None
-            # NOTE doing so was creating an inconsistent number of available features
-            # which was expected, but most models I've tested can't handle this
-            df.drop(columns=["Source_IP", "Destination_IP"], axis=1, inplace=True) # drop IP addresses to avoid using them in the models
-            df.drop(columns=["Packet_no"], axis=1, inplace=True) # drop packet number to avoid using it in the models
-            
-            # Encode the categorical features
-            feature_encoder = OrdinalEncoder()
-            for col in df.columns:
-                if col == "TCP_compl_str" or col == "TCP_flags_str" or col == "Frame_protocols":
-                    df[col] = feature_encoder.fit_transform(df[[col]])
-            
-            # Change None (NaN type) to an int
-            df.fillna(-1, inplace=True) # required by models like LR that can't handle NaN data
-            # NOTE -1 is a valid int while not representing valid data (which starts with 0)
-            # df.dropna(inplace=True) # not required for now (see "fillna line" above)
-
-            # Normalize features using standardization
-            # using MinMax to avoid giving more importance to a given feature
-            # for more information: https://scikit-learn.org/stable/auto_examples/preprocessing/plot_all_scaling.html
-            scaler = MinMaxScaler() 
-            data_features_names = list(df)
-            scaler.fit(df[data_features_names])
-            df[data_features_names] = scaler.transform(df[data_features_names])
-            preprocess_time_end = time_ns()
-
-            # Save the normalized data
-            preprocess_disk_time_begin = time_ns()
-            df.to_csv(out_file_path, index=False)
-            preprocess_disk_time_end = time_ns()
-            print("[INFO] Preprocessed data saved to:", out_file_path)
-            
-            preprocess_disk_time_ms = (preprocess_disk_time_end - preprocess_disk_time_begin) / 10**6
-            preprocess_time_ms = (preprocess_time_end - preprocess_time_begin) / 10**6
-            preprocess_total_time_ms = (preprocess_time_ms + preprocess_disk_time_ms)
-
-            new_row = {
-                    columns_preprocess_time_df[0]: file_name,
-                    columns_preprocess_time_df[1]: data_num_rows,
-                    columns_preprocess_time_df[2]: preprocess_time_ms,
-                    columns_preprocess_time_df[3]: preprocess_disk_time_ms,
-                    columns_preprocess_time_df[4]: preprocess_total_time_ms,
-                }
-                
-            # Add new row to the dataframe using loc[] method
-            preprocess_time_df.loc[len(preprocess_time_df)] = new_row
-        else:
-            print(f"[INFO] The file {file_name} was already preprocessed")
-
-    if len(preprocess_time_df) != 0:
-        preprocess_time_df.to_csv(f"{output_files_path_results}{timestamp}_preprocess_time.csv", index=False)
-
 def train_models(model, X_train, X_test, y_train, y_test):
     model_name = model.__class__.__name__
     data_num_rows = len(X_train)
@@ -243,7 +142,7 @@ def train_models(model, X_train, X_test, y_train, y_test):
 csv_files = glob_get_files_list(input_files_path, "csv")
 
 # Preprocess the data
-preprocess_data(csv_files, output_files_path_preprocessed_data)
+preprocess_data(csv_files, output_files_path_preprocessed_data, output_files_path_results, timestamp)
 
 # Update the list of CSV files
 csv_files = glob_get_files_list(output_files_path_preprocessed_data, "csv")
--- util.py
+++ util.py
@@ -2,6 +2,9 @@ import pandas as pd
 import glob
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+from time import time_ns
+from sklearn.preprocessing import MinMaxScaler,OrdinalEncoder
 
 def read_csv(file_path):
     """
@@ -136,3 +139,130 @@ def plot_confusion_matrix(matrix, output_dir, data_input_file_name, model_name, 
     elif (show_plot):
         plt.show() # DEBUG
     plt.close() # close figure to be able to plot other iterations correctly
+
+def preprocess_data(files_path, preprocessed_data_output_dir, results_output_dir, timestamp):
+    """
+    A function to preprocess the data before labeling it and running the train and inference steps of the ML pipeline.
+
+     Parameters
+    ----------
+        files_path: string
+            A path that points to where the input files are located and should be read.
+        preprocessed_data_output_dir: string
+            The output path where the preprocessed data file should be saved.
+        results_output_dir: string
+            The output path where the results (e.g. file name, num of rows and process time) file should be saved.
+        timestamp: string
+            A datetime-like formatted string that represents the time when this plot's data was created.
+    """
+    columns_preprocess_time_df = ["file_name", "file_num_rows", "preprocess_time_ms", "preprocess_disk_time_ms", "preprocess_total_time_ms"]
+    preprocess_time_df = pd.DataFrame(columns=columns_preprocess_time_df) # df to save the results
+
+    for i in files_path:
+        file_name = i.split('/')[-1]
+        out_file_path = preprocessed_data_output_dir + file_name
+        preprocess_time_begin = time_ns()
+
+        if not os.path.exists(out_file_path):
+            df = read_csv(i)
+            data_num_rows = len(df)
+            print("[INFO] Working with", file_name)
+
+            # Drop some data
+            # df = df.dropna(axis=1, how='all') # drop columns where all values are None
+            # NOTE doing so was creating an inconsistent number of available features
+            # which was expected, but most models I've tested can't handle this
+            df.drop(columns=["Source_IP", "Destination_IP"], axis=1, inplace=True) # drop IP addresses to avoid using them in the models
+            df.drop(columns=["Packet_no"], axis=1, inplace=True) # drop packet number to avoid using it in the models
+            
+            # Encode the categorical features
+            feature_encoder = OrdinalEncoder()
+            for col in df.columns:
+                if col == "TCP_compl_str" or col == "TCP_flags_str" or col == "Frame_protocols":
+                    df[col] = feature_encoder.fit_transform(df[[col]])
+            
+            # Change None (NaN type) to an int
+            df.fillna(-1, inplace=True) # required by models like LR that can't handle NaN data
+            # NOTE -1 is a valid int while not representing valid data (which starts with 0)
+            # df.dropna(inplace=True) # not required for now (see "fillna line" above)
+
+            # Normalize features using standardization
+            # using MinMax to avoid giving more importance to a given feature
+            # for more information: https://scikit-learn.org/stable/auto_examples/preprocessing/plot_all_scaling.html
+            scaler = MinMaxScaler() 
+            data_features_names = list(df)
+            scaler.fit(df[data_features_names])
+            df[data_features_names] = scaler.transform(df[data_features_names])
+            preprocess_time_end = time_ns()
+
+            # Save the normalized data
+            preprocess_disk_time_begin = time_ns()
+            df.to_csv(out_file_path, index=False)
+            preprocess_disk_time_end = time_ns()
+            print("[INFO] Preprocessed data saved to:", out_file_path)
+            
+            preprocess_disk_time_ms = (preprocess_disk_time_end - preprocess_disk_time_begin) / 10**6
+            preprocess_time_ms = (preprocess_time_end - preprocess_time_begin) / 10**6
+            preprocess_total_time_ms = (preprocess_time_ms + preprocess_disk_time_ms)
+
+            new_row = {
+                    columns_preprocess_time_df[0]: file_name,
+                    columns_preprocess_time_df[1]: data_num_rows,
+                    columns_preprocess_time_df[2]: preprocess_time_ms,
+                    columns_preprocess_time_df[3]: preprocess_disk_time_ms,
+                    columns_preprocess_time_df[4]: preprocess_total_time_ms,
+                }
+                
+            # Add new row to the dataframe using loc[] method
+            preprocess_time_df.loc[len(preprocess_time_df)] = new_row
+        else:
+            print(f"[INFO] The file {file_name} was already preprocessed")
+
+    if len(preprocess_time_df) != 0:
+        preprocess_time_df.to_csv(f"{results_output_dir}{timestamp}_preprocess_time.csv", index=False)
+
+# Function to read and label data in CSV files
+def read_and_label_data(file_path, out_dir, remove_old_files=False):
+    """
+    A function to label the data before running the train and inference steps of the ML pipeline.
+
+     Parameters
+    ----------
+        file_path: string
+            A path that points to where the input file is located on disk.
+        out_dir: string
+            A path where the labelled data should be stored on.
+        remove_old_files: bool
+            If True, the input file is deleted after processing.
+    """
+    # Read the CSV file
+    file_name_without_path = file_path.split('/')[-1] # get old file name
+    new_file_name_and_path = out_dir + os.path.splitext(file_name_without_path)[0] + "_labeled.csv"
+    if not os.path.exists(new_file_name_and_path):
+        df = pd.read_csv(file_path)
+        
+        # Assign labels based on filenames
+        if 'embb' in file_path:
+            label = 0
+        elif 'urllc' in file_path:
+            label = 1
+        elif 'mmtc' in file_path:
+            label = 2
+        else:
+            raise ValueError(f"Could not determine label from filename {file_path}")
+            exit()
+        
+        
+        # Add label to DataFrame
+        print(f"[INFO] Labeling {file_name_without_path} ... ", end='')
+        df['label'] = label
+        df.to_csv(new_file_name_and_path, index=False)
+        print("[ OK ]")
+
+        # if storage space is constrained, update remove_old_files to True to free some space
+        if (remove_old_files):
+            delete_files(file_name_without_path, file_path)
+        
+        print("[DEBU] Labeled data successfully saved to", new_file_name_and_path) # DEBUG
+    else:
+        print(f"[INFO] The file {file_name_without_path} was already labeled")
