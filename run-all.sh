bbd5e94070ee7fe5c88c5accf8bf02c1f81f0fde
oliveiraleo <oliveiraleo@users.noreply.github.com>
2025-07-22 11:55:35 -0300

    feat: add the `run-all.sh` script

---
+++ run-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+bash pcap_extract.sh
+python dataset_CSV_characterization.py
+python stat-plotter.py
+
+
+python export_JSON.py
+python box-plotter.py
+bash add_label_to_name.sh
+
+python ml.py
+
+python inference.py
